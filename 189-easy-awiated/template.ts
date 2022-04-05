2900330cf0e0ed58aa63abd5b36689ce7345ebd7
Tdyh <cheng.cui@ecs.co.uk>
2022-04-05 15:47:32 +0100

    for test git

--- 189-easy-awiated/template.ts
+++ 189-easy-awiated/template.ts
@@ -8,7 +8,7 @@ type MyAwaited<T extends Promise<any>> = T extends Promise<infer P>
     ? MyAwaited<P>
     : P
   : never;
-
+//for test
 // 全靠三目运算符  完成各种if 语句 最上面内种错误的原因是因为最后一次 的条件不满足  学到了！！！
 type myX = Promise<Promise<string>>;
 
